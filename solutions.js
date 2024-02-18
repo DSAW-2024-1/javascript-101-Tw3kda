b9e7a6545f9c97ddb5c42899f8204fcb71d28df0
Tw3kda <santiago2006nc@gmail.com>
2024-02-18 17:46:04 -0500

    primer intento

--- solutions.js
+++ solutions.js
@@ -1,27 +1,97 @@
 // Sum of Two Numbers
 function sum(a, b) {
-  // Add your code here
+  if(!isNaN(a) || !isNaN(b) ){
+    console.log("Los datos de entrada no son adecuados")
+    
+  }else{
+    let c = 0
+    c= a+b 
+    return(c)
+    console.log(c)
+
+  }
+ 
 }
 
 // Factorial of a Number
 function factorial(n) {
-  // Add your code here
+  let resul = 0
+  if(!isNaN(n)){
+    console.log("Los datos de entrada no son adecuados")
+  }else{
+    for(n;n=>1; n-- ){
+      resul *= n
+      return(resul)
+      console.log(resul)
+   }
+
+  }
+  
 }
 
 // Find the Largest Number
 function findLargest(arr) {
-  // Add your code here
+  if(!Array.isArray(arr) || !isNaN(arr)){
+    console.log("Los datos de entrada no son adecuados")
+  }else{
+    let mayor = arr[0];
+    for(let i = 0; i < myArray.length; i++){
+    
+    if(arr[i+1]>arr[i]){
+      mayor = arr[i+1]
+    }
+  }
+  console.log(mayor)
+  return(mayor)
+
+  }
+  
 }
 
 // Count Vowels in a String
 function countVowels(str) {
-  // Add your code here
+  if(typeof str != 'string'){
+    console.log("Los datos de entrada no son adecuados")
+  }else{
+    let count = 0;
+  
+    for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+    
+    }
+    return count;
+    console.log(count)
+
+  }
+  
 }
 
 // Check if a Number is Prime
 function isPrime(n) {
-  // Add your code here
+  
+  if(!isNaN(n)){
+    console.log("Los datos de entrada no son adecuados")
+  }else{
+    if (number <= 1) {
+      return false;
+    }
+
+  
+  for (let i = 2; i <= Math.sqrt(number); i++) 
+  {
+      if (number % i == 0) {
+          return false; 
+      }
+  }
+
+  return true; 
+  }
 }
+
+  
+
 
 module.exports = {
   sum,
